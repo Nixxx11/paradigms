9bcc07e87341aab14d319b3769a8e1e4670aaee3
Gorbunov_Nikita <368044@niuitmo.ru>
2023-02-13 23:18:06 +0300

    Small fixes in comments

--- java-solutions/search/BinarySearch.java
+++ java-solutions/search/BinarySearch.java
@@ -6,7 +6,7 @@ public class BinarySearch {
     // Post: left <= R <= right
     //  && forall i = left...(R - 1): a[i] > x
     //  && (R == right || a[R] <= x)
-    //  && forall n in a: n' == n
+    //  && a' == a
     public static int iterativeSearch(int[] a, int x, int left, int right) {
         // Inv: 0 <= left <= left' <= right' <= right <= a.length
         //  && (left' == left || a[left' - 1] > x)
@@ -43,7 +43,7 @@ public class BinarySearch {
                 //  && (right' == right || a[right'] <= x)
                 //  && forall i = (left + 1)...(right - 1): a[i - 1] >= a[i]
             }
-            // 0 <= left <= left' < right' <= right <= a.length
+            // 0 <= left <= left' <= right' <= right <= a.length
             //  && (left' == left || a[left' - 1] > x)
             //  && (right' == right || a[right'] <= x)
             //  && forall i = (left + 1)...(right - 1): a[i - 1] >= a[i]
@@ -63,7 +63,7 @@ public class BinarySearch {
     // Post: 0 <= R <= a.length
     //  && forall i = 0...(R - 1): a[i] > x
     //  && (R == a.length || a[R] <= x)
-    //  && forall n in a: n' == n
+    //  && a' == a
     public static int iterativeSearch(int[] a, int x) {
         // forall i = 1...(a.length - 1): a[i - 1] >= a[i]
         return iterativeSearch(a, x, 0, a.length);
@@ -71,7 +71,7 @@ public class BinarySearch {
         //  && forall i = 0...(R - 1): a[i] > x
         //  && (R == a.length || a[R] <= x)
         //  && forall i = 1...(a.length - 1): a[i - 1] >= a[i]
-        //  && forall n in a: n' == n
+        //  && a' == a
     }
 
     // Pre: 0 <= left <= right <= a.length
@@ -79,7 +79,7 @@ public class BinarySearch {
     // Post: left <= R <= right
     //  && forall i = left...(R - 1): a[i] > x
     //  && (R == right || a[R] <= x)
-    //  && forall n in a: n' == n
+    //  && a' == a
     public static int recursiveSearch(int[] a, int x, int left, int right) {
         // 0 <= left <= right <= a.length
         //  && forall i = (left + 1)...(right - 1): a[i - 1] >= a[i]
@@ -91,7 +91,7 @@ public class BinarySearch {
             //  && R == right
             //  && forall i = (left + 1)...(right - 1): a[i - 1] >= a[i]
         }
-        // 0 <= left <= right <= a.length
+        // 0 <= left < right <= a.length
         //  && forall i = (left + 1)...(right - 1): a[i - 1] >= a[i]
         int middle = (left + right) / 2;
         // 0 <= left <= middle < right <= a.length
@@ -101,7 +101,7 @@ public class BinarySearch {
             //  && a[middle] <= x
             //  && forall i = (left + 1)...(right - 1): a[i - 1] >= a[i]
             return recursiveSearch(a, x, left, middle);
-            // left <= R <= middle < right
+            // 0 <= left <= R <= middle < right <= a.length
             //  && forall i = left...(R - 1): a[i] > x
             //  && (R == middle || a[R] <= x)   // a[R] <= x
             //  && forall i = (left + 1)...(right - 1): a[i - 1] >= a[i]
@@ -110,7 +110,7 @@ public class BinarySearch {
             //  && a[middle] > x    // forall i = left...middle: a[i] > x
             //  && forall i = (left + 1)...(right - 1): a[i - 1] >= a[i]
             return recursiveSearch(a, x, middle + 1, right);
-            // left < middle + 1 <= R <= right
+            // 0 <= left < middle + 1 <= R <= right <= a.length
             //  && forall i = left...middle: a[i] > x
             //  && forall i = (middle + 1)...(R - 1): a[i] > x
             //  && (R == right || a[R] <= x)
@@ -122,7 +122,7 @@ public class BinarySearch {
     // Post: 0 <= R <= a.length
     //  && forall i = 0...(R - 1): a[i] > x
     //  && (R == a.length || a[R] <= x)
-    //  && forall n in a: n' == n
+    //  && a' == a
     public static int recursiveSearch(int[] a, int x) {
         // forall i = 1...(a.length - 1): a[i - 1] >= a[i]
         return recursiveSearch(a, x, 0, a.length);
@@ -130,7 +130,7 @@ public class BinarySearch {
         //  && forall i = 0...(R - 1): a[i] > x
         //  && (R == a.length || a[R] <= x)
         //  && forall i = 1...(a.length - 1): a[i - 1] >= a[i]
-        //  && forall n in a: n' == n
+        //  && a' == a
     }
 
     // Pre: args.length >= 1 && forall arg in args: arg can be parsed to integer
@@ -143,30 +143,30 @@ public class BinarySearch {
         //  && forall i = 2...(args.length - 1): Integer.parseInt(args[i - 1]) >= Integer.parseInt(args[i])
         int x = Integer.parseInt(args[0]);
         int[] a = new int[args.length - 1];
-        int i = 1;
+        int j = 1;
         // forall arg in args: arg can be parsed to integer
         // Inv: x == Integer.parseInt(args[0])
         //  && a.length == args.length - 1
-        //  && forall j = 0...(i - 2): a[j] == Integer.parseInt(args[j + 1])
-        //  && forall j = 2...(args.length - 1): Integer.parseInt(args[j - 1]) >= Integer.parseInt(args[j])
-        while (i < args.length) {
-            a[i - 1] = Integer.parseInt(args[i]);
-            i++;
+        //  && forall i = 0...(j - 2): a[i] == Integer.parseInt(args[i + 1])
+        //  && forall i = 2...(args.length - 1): Integer.parseInt(args[i - 1]) >= Integer.parseInt(args[i])
+        while (j < args.length) {
+            a[j - 1] = Integer.parseInt(args[j]);
+            j++;
         }
         // x == Integer.parseInt(args[0])
         //  && a.length == args.length - 1
-        //  && forall j = 0...(args.length - 2): a[j] == Integer.parseInt(args[j + 1])
-        //  && forall j = 2...(args.length - 1): Integer.parseInt(args[j - 1]) >= Integer.parseInt(args[j])
+        //  && forall i = 0...(args.length - 2): a[i] == Integer.parseInt(args[i + 1])
+        //  && forall i = 2...(args.length - 1): Integer.parseInt(args[i - 1]) >= Integer.parseInt(args[i])
 
         // x == Integer.parseInt(args[0])
         //  && a.length == args.length - 1
-        //  && forall j = 0...(args.length - 2): a[j] == Integer.parseInt(args[j + 1])
-        //  && forall j = 1...(a.length - 1): a[j - 1] >= a[j]
+        //  && forall i = 0...(args.length - 2): a[i] == Integer.parseInt(args[i + 1])
+        //  && forall i = 1...(a.length - 1): a[i - 1] >= a[i]
         System.out.println(iterativeSearch(a, x));
         // 0 <= R <= a.length
         //  && forall i = 0...(R - 1): a[i] > x
         //  && (R == a.length || a[R] <= x)
-        //  && forall j = 0...(args.length - 2): a[j] == Integer.parseInt(args[j + 1])
+        //  && forall i = 0...(args.length - 2): a[i] == Integer.parseInt(args[i + 1])
 
         // 0 <= R <= args.length - 1
         //  && forall i = 0...(R - 1): Integer.parseInt(args[i + 1]) > Integer.parseInt(args[0])
