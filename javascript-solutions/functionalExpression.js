ab753c2065d5567c2499c008cf5c13ceacfb4f82
Gorbunov_Nikita <368044@niuitmo.ru>
2023-03-13 21:18:40 +0300

    Replaced big switch with map

--- javascript-solutions/functionalExpression.js
+++ javascript-solutions/functionalExpression.js
@@ -27,34 +27,34 @@ const multiply = makeOperation((n1, n2) => n1 * n2);
 const divide = makeOperation((n1, n2) => n1 / n2);
 const negate = makeOperation((n) => -n);
 
-function parseOperation(string, getOperands) {
-    switch (string) {
-        case "x":
-        case "y":
-        case "z":
-            return variable(string);
-        case "negate":
-            return negate(...getOperands(1));
-        case "+":
-            return add(...getOperands(2));
-        case "-":
-            return subtract(...getOperands(2));
-        case "*":
-            return multiply(...getOperands(2));
-        case "/":
-            return divide(...getOperands(2));
-        default:
-            return cnst(parseInt(string));
+const map = {
+    "x": [variable("x"), 0],
+    "y": [variable("y"), 0],
+    "z": [variable("z"), 0],
+    "negate": [negate, 1],
+    "+": [add, 2],
+    "-": [subtract, 2],
+    "*": [multiply, 2],
+    "/": [divide, 2]
+};
+
+function parseOperation(string, makeExpression) {
+    if (string in map) {
+        return makeExpression(...map[string]);
+    } else {
+        return cnst(parseInt(string));
     }
 }
 
 function parse(expression) {
     const tokens = expression.split(" ");
     const stack = [];
-    const getOperands = (number) => stack.splice(-number);
+    const makeExpression = (operation, numberOfOperands) => {
+        return numberOfOperands === 0 ? operation : operation(...stack.splice(-numberOfOperands));
+    }
     for (const token of tokens) {
         if (token.length !== 0) {
-            stack.push(parseOperation(token, getOperands));
+            stack.push(parseOperation(token, makeExpression));
         }
     }
     return stack.pop();
