babd064a29a05914fbb2ec664f798b0dac1c3eea
Gorbunov_Nikita <368044@niuitmo.ru>
2023-03-23 13:05:00 +0300

    Added prefix homework

--- javascript-solutions/objectExpression.js
+++ javascript-solutions/objectExpression.js
@@ -3,19 +3,20 @@
 const Const = function (number) {
     this.number = number;
 }
-const zero = new Const(0);
-const half = new Const(0.5);
-const one = new Const(1);
-const two = new Const(2);
 Const.prototype.evaluate = function (...vars) {
     return this.number;
 };
 Const.prototype.diff = function (varName) {
     return zero;
 };
-Const.prototype.toString = function () {
+Const.prototype.prefix = Const.prototype.toString = function () {
     return this.number.toString();
 };
+
+const zero = new Const(0);
+const half = new Const(0.5);
+const one = new Const(1);
+const two = new Const(2);
 
 const Variable = function (name) {
     this.name = name;
@@ -31,7 +32,7 @@ Variable.prototype.evaluate = function (...vars) {
 Variable.prototype.diff = function (varName) {
     return varName === this.name ? one : zero;
 };
-Variable.prototype.toString = function () {
+Variable.prototype.prefix = Variable.prototype.toString = function () {
     return this.name;
 };
 
@@ -45,6 +46,9 @@ const expressions = (function () {
     }
     AbstractOperation.prototype.toString = function () {
         return this.operands.join(" ") + " " + this.symbol;
+    }
+    AbstractOperation.prototype.prefix = function () {
+        return "(" + this.symbol + " " + this.operands.map((expression) => expression.prefix()).join(" ") + ")";
     }
 
     const operationsList = [];
@@ -94,20 +98,20 @@ const Multiply = makeOperation((n1, n2) => n1 * n2, "*",
 
 const Divide = makeOperation((n1, n2) => n1 / n2, "/",
     function (varName) {
-        return new Divide(
-            new Subtract(
-                new Multiply(
-                    this.operands[0].diff(varName),
-                    this.operands[1]
-                ),
+        return new Subtract(
+            new Divide(
+                this.operands[0].diff(varName),
+                this.operands[1]
+            ),
+            new Divide(
                 new Multiply(
                     this.operands[0],
                     this.operands[1].diff(varName)
+                ),
+                new Multiply(
+                    this.operands[1],
+                    this.operands[1]
                 )
-            ),
-            new Multiply(
-                this.operands[1],
-                this.operands[1]
             )
         );
     }
@@ -149,7 +153,7 @@ const distances = (function () {
 const [Sumsq2, Sumsq3, Sumsq4, Sumsq5] = distances.sumSq;
 const [Distance2, Distance3, Distance4, Distance5] = distances.distance;
 
-const parse = (function () {
+const parser = (function () {
     const symbols = Object.keys(Variable.names).reduce(
         (map, varName) => ({...map, [varName]: new Variable(varName)}),
         {}
@@ -158,29 +162,105 @@ const parse = (function () {
         (map, constructor) => ({...map, [constructor.prototype.symbol]: constructor}),
         {}
     );
-    const curry = (f) => (arg) => (...args) => f(arg, ...args);
 
-    function parseOperation(makeExpression, string) {
-        if (string in symbols) {
-            return symbols[string];
-        } else if (string in operations) {
-            return makeExpression(operations[string]);
-        } else {
-            return new Const(parseInt(string));
+    function ParsingError(message) {
+        this.message = message;
+    }
+
+    ParsingError.prototype = Object.create(Error.prototype);
+    ParsingError.prototype.name = "ParsingError";
+    ParsingError.prototype.constructor = ParsingError;
+
+    function check(condition, position, errorMessage) {
+        if (!condition) {
+            throw new ParsingError("Token " + position + ": " + errorMessage);
         }
     }
 
-    return function (expression) {
-        const tokens = expression.split(" ");
+    function parseSuffix(expression) {
+        const tokens = expression.split(" ").filter((token) => token.length > 0);
         const stack = [];
-        const parseWithStack = curry(parseOperation)(
-            (operation) => new operation(...stack.splice(-operation.argsCount))
-        );
-        for (const token of tokens) {
-            if (token.length !== 0) {
-                stack.push(parseWithStack(token));
+        for (let i = 0; i < tokens.length; i++) {
+            const token = tokens[i];
+            if (token in symbols) {
+                stack.push(symbols[token]);
+            } else if (token in operations) {
+                const operation = operations[token];
+                check(
+                    stack.length >= operation.argsCount,
+                    i, "'" + operation.prototype.symbol + "' does not have enough operands"
+                );
+                stack.push(new operation(...stack.splice(-operation.argsCount)));
+            } else {
+                const number = parseInt(token);
+                check(number.toString() === token, i, token + " is not a valid token");
+                stack.push(new Const(number));
             }
         }
-        return stack.pop();
-    };
+        const result = stack.pop();
+        check(stack.length === 0, tokens.length, "Excess tokens: " + stack);
+        return result;
+    }
+
+    const parentheses = {"(": "(", ")": ")"};
+
+    function splitWithParentheses(str) {
+        const result = [];
+        for (let pos = 0; pos < str.length; pos++) {
+            if (str.charAt(pos) in parentheses) {
+                result.push(str.charAt(pos));
+            } else if (str.charAt(pos) !== " ") {
+                const from = pos;
+                while (pos + 1 < str.length && !(str.charAt(pos + 1) in parentheses) && str.charAt(pos + 1) !== " ") {
+                    pos++;
+                }
+                result.push(str.substring(from, pos + 1));
+            }
+        }
+        return result;
+    }
+
+    function parsePrefix(expression) {
+        const tokens = splitWithParentheses(expression);
+        let i = 0;
+
+        function parseOperation() {
+            const token = tokens[i];
+            i++;
+            if (token === "(") {
+                check(i < tokens.length, i, "'(' at the end of the string");
+                check(tokens[i] in operations, i, "Unknown operation: " + tokens[i]);
+                const operation = operations[tokens[i]];
+                const args = [];
+                i++;
+                while (i < tokens.length && tokens[i] !== ")") {
+                    args.push(parseOperation());
+                }
+                check(tokens[i] === ")", i, "No ')' for " + operation.prototype.symbol);
+                check(
+                    operation.argsCount === args.length,
+                    i,
+                    operation.argsCount + " arguments expected for '" + operation.prototype.symbol + "', got " + args.length
+                );
+                i++;
+                return new operation(...args);
+            } else {
+                if (token in symbols) {
+                    return symbols[token];
+                } else {
+                    const number = parseInt(token);
+                    check(number.toString() === token, i, token + " is not a valid token");
+                    return new Const(number);
+                }
+            }
+        }
+
+        const result = parseOperation();
+        check(i === tokens.length, i, "Excess tokens: " + tokens.slice(i));
+        return result;
+    }
+
+    return {parseSuffix, parsePrefix, ParsingError};
 })();
+const parse = parser.parseSuffix;
+const parsePrefix = parser.parsePrefix;
