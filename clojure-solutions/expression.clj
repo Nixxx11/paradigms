7095caf2e6d3bc6fccce7e06b8dd82cc877790ca
Gorbunov_Nikita <368044@niuitmo.ru>
2023-04-24 21:28:12 +0300

    Added objects homework (easy)

--- clojure-solutions/expression.clj
+++ clojure-solutions/expression.clj
@@ -17,16 +17,59 @@
 (defn lse-operator [& args] (Math/log (apply sumexp-operator args)))
 (def lse (make-operation lse-operator))
 
-(def operation-map {'+      add,
-                    '-      subtract,
-                    '*      multiply,
-                    '/      divide,
-                    'negate negate,
-                    'sumexp sumexp,
-                    'lse    lse})
+(def function-map {'+      add,
+                   '-      subtract,
+                   '*      multiply,
+                   '/      divide,
+                   'negate negate,
+                   'sumexp sumexp,
+                   'lse    lse})
 
-(defn parse [expr] (cond
-                     (list? expr) (apply (operation-map (first expr)) (map parse (rest expr)))
-                     (number? expr) (constant expr)
-                     :else (variable (name expr))))
-(defn parseFunction [string] (parse (read-string string)))
+(load-file "proto.clj")
+(defclass
+  Variable
+  _
+  [name]
+  [toString [] (__name this)]
+  [evaluate [vars] (vars (__name this))])
+(defclass
+  Constant
+  _
+  [num]
+  [toString [] (str (__num this))]
+  [evaluate [vars] (__num this)])
+
+(defmethods getSymbol, calculate)
+(defclass
+  AbstractOperation
+  _
+  [operands]
+  [toString [] (str "(" (_getSymbol this) " " (clojure.string/join " " (map _toString (__operands this))) ")")]
+  [evaluate [vars] (_calculate this (map #(_evaluate % vars) (__operands this)))])
+
+(def object-map {})
+(defn make-operation-class [func symb]
+  (let [OperationPrototype (assoc AbstractOperation_proto :getSymbol (fn [this] symb)
+                                                          :calculate (fn [this nums] (apply func nums)))
+        OperationConstructor (constructor (fn [this & operands] (assoc this :operands operands)) OperationPrototype)]
+    (def object-map (assoc object-map (symbol symb) OperationConstructor))
+    OperationConstructor))
+
+(def Add (make-operation-class + "+"))
+(def Subtract (make-operation-class - "-"))
+(def Multiply (make-operation-class * "*"))
+(def Divide (make-operation-class fixed-div "/"))
+(def Negate (make-operation-class - "negate"))
+
+(defn make-parser [operations-map const-func var-func]
+  (fn parse [expr] (cond
+                     (list? expr) (apply (operations-map (first expr)) (map parse (rest expr)))
+                     (number? expr) (const-func expr)
+                     :else (var-func (name expr)))))
+(def function-parser (make-parser function-map constant variable))
+(def object-parser (make-parser object-map Constant Variable))
+(defn parseFunction [string] (function-parser (read-string string)))
+(defn parseObject [string] (object-parser (read-string string)))
+
+(def toString _toString)
+(def evaluate _evaluate)
