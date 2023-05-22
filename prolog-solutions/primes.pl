e4e348912d807073fb48fe2f34a572f6e9f82ad4
Gorbunov_Nikita <368044@niuitmo.ru>
2023-05-22 22:50:17 +0300

    Revert changes

--- prolog-solutions/primes.pl
+++ prolog-solutions/primes.pl
@@ -1,61 +1,70 @@
-composite(N) :- lowest_prime_divisor(N, _).
+composite(N) :- composites_table(N, _).
 
 prime(N) :- N > 1, \+ composite(N).
 
-prime_divisors(1, []).
-prime_divisors(N, [N]) :- prime(N).
+prime_divisors(1, []) :- !.
+
+prime_divisors(N, [N]) :- prime(N), !.
+
 prime_divisors(N, [Lowest_divisor | Rest_divisors]) :-
-	number(N), !,
-	lowest_prime_divisor(N, Lowest_divisor),
-	Quotient is N / Lowest_divisor,
-	prime_divisors(Quotient,  Rest_divisors).
+		number(N),
+		composites_table(N, Lowest_divisor),
+		Quotient is N / Lowest_divisor,
+		prime_divisors(Quotient,  Rest_divisors), !.
+
 prime_divisors(N, [Lowest_divisor | Rest_divisors]) :-
-	prime_divisors(Quotient,  Rest_divisors),
-	Product is Quotient * Lowest_divisor,
-	lowest_prime_divisor(Product, Lowest_divisor),
-	N = Product.
+		number(Lowest_divisor),
+		prime_divisors(Quotient,  Rest_divisors),
+		Product is Quotient * Lowest_divisor,
+		composites_table(Product, Lowest_divisor),
+		N = Product, !.
 
 first(N, [N | _]).
 
 convert_to_pairs([], []).
+
 convert_to_pairs([First | Rest], [(First, 1) | Rest_p]) :-
-	convert_to_pairs(Rest, Rest_p),
-	\+ first(First, Rest), !.
+		convert_to_pairs(Rest, Rest_p), 
+		\+ first(First, Rest), !.
+
 convert_to_pairs([First | Rest], [(First, Count) | Rest_p]) :-
-	number(First), !,
-	convert_to_pairs(Rest, [(First, Count1) | Rest_p]),
-	Count is Count1 + 1.
+		number(First),
+		convert_to_pairs(Rest, [(First, Count1) | Rest_p]),
+		Count is Count1 + 1, !.
+
 convert_to_pairs([First | Rest], [(First, Count) | Rest_p]) :-
-	Count1 is Count - 1,
-	convert_to_pairs(Rest, [(First, Count1) | Rest_p]).
+		number(Count),
+		Count1 is Count - 1,
+		convert_to_pairs(Rest, [(First, Count1) | Rest_p]).
 
 compact_prime_divisors(N, CDs) :- 
-	number(N), !,
-	prime_divisors(N, D),
-	convert_to_pairs(D, CDs).
+		number(N),
+		prime_divisors(N, D), 
+		convert_to_pairs(D, CDs), !.
+
 compact_prime_divisors(N, CDs) :- 
-	convert_to_pairs(D, CDs),
-	prime_divisors(N, D).
+		convert_to_pairs(D, CDs),
+		prime_divisors(N, D), !.
 
-check_multiple(N, A, 0) :- 
-	A mod N =:= 0,
-	assert(lowest_prime_divisor(A, N)), !.
-check_multiple(N, A, A).
+update_table(Prime, Composite, Limit) :- Composite > Limit, !.
 
-positive(N) :- N > 0.
+update_table(Prime, Composite, Limit) :-
+		prime(Composite),
+		assert(composites_table(Composite, Prime)).
 
-filter_multiples(_, [], []).
-filter_multiples(N, [H | T], [H1 | R]) :-
-	check_multiple(N, H, H1),
-	filter_multiples(N, T, R).
+update_table(Prime, Composite, Limit) :-
+		Composite1 is Composite + Prime,
+		update_table(Prime, Composite1, Limit).
 
-filter_composites([H | T], Max) :-
-	H =< Max,
-	filter_multiples(H, T, R),
-	findall(Num, (member(Num, R), positive(Num)), R1),
-	filter_composites(R1, Max).
+update_table_iterative(I, Square, Limit) :- Square > Limit, !.
 
-range(L, L, []).
-range(N, L, [N | T]) :- N < L, N1 is N + 1, range(N1, L, T).
+update_table_iterative(I, Square, Limit) :-
+		prime(I),
+		update_table(I, Square, Limit).
 
-init(MAX_N) :- range(2, MAX_N, R), S is sqrt(MAX_N), filter_composites(R, S).
+update_table_iterative(I, Square, Limit) :-
+		I1 is I + 1,
+		Square1 is Square + I + I + 1,
+		update_table_iterative(I1, Square1, Limit).
+
+init(MAX_N) :- update_table_iterative(2, 4, MAX_N), fail.
